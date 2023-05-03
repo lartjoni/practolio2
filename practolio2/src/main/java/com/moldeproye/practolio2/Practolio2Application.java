c6f713c872b9fd9fa85e9e556ef49ca89808733e
lartjoni <lartoniron@gmail.com>
2023-05-03 00:40:53 -0300

    practica practolio2

---
+++ practolio2/src/main/java/com/moldeproye/practolio2/Practolio2Application.java
@@ -0,0 +1,13 @@
+package com.moldeproye.practolio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practolio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practolio2Application.class, args);
+	}
+
+}
---
+++ practolio2/src/test/java/com/moldeproye/practolio2/Practolio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldeproye.practolio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practolio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
